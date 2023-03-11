ca69503f75ff88ac671f4aa82acc26fd1ed7b536
evmorozovvv <116379756+evmorozovvv@users.noreply.github.com>
2023-03-11 19:08:42 +0300

    Программа работает
    куча лишних файлов потому что перемещала в другую папку..

---
+++ прога/Program.cs
@@ -0,0 +1,31 @@
+﻿
+string[] array = { "qwerty", "agр", ":-)", "1и62", "контрольная" };
+string[] arrayResult = new string[array.Length];
+string empty = string.Empty;
+
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+       for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i] != empty)
+            Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i].Length < 4)
+        {
+            arrayResult[i] = array[i];
+        }
+        else
+            arrayResult[i] = empty;
+    }
+
+
+    PrintArray(arrayResult);
+
+
